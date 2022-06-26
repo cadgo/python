eee1e2a91d7569499a3d107fa69e36eda2342804
Carlos Diaz <cadgo.root@gmail.com>
2022-06-26 03:44:33 +0000

    jumplin

---
+++ pygame/jumpling.py
@@ -0,0 +1,33 @@
+import pygame, sys
+pygame.init()
+
+clock = pygame.time.Clock()
+screen_width,screen_high = 800,800
+floor_position = screen_high - 200
+
+main_char_size=50
+main_char_speed=10
+
+main_char_size = 50
+main_char = pygame.Rect(screen_width/2,\
+        floor_position-main_char_size,\
+        main_char_size,main_char_size)
+screen = pygame.display.set_mode((screen_width, screen_high))
+isRunning=True
+while isRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if (event.key == pygame.K_LEFT or event.key == ord('a')) and main_char.x > 0:
+                print("moviendo izq x", main_char.x)
+                main_char.x -= main_char_speed
+            if (event.key == pygame.K_RIGHT or event.key == ord('b')) and main_char.x+main_char_size < screen_width:
+                print("Moviendo derecha", main_char.x)
+                main_char.x += main_char_speed
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen,(255,255,255),main_char)
+    pygame.draw.line(screen, (64,64,64),
+            (0,floor_position),(screen_width,floor_position),10)
+    pygame.display.flip()
+    clock.tick(60)
