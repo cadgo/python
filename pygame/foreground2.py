0b4d0adefed83dc028d660039c63bfbc05cde828
Carlos Diaz <cadgo.root@gmail.com>
2023-06-06 23:41:09 -0500

    test

--- pygame/foreground2.py
+++ pygame/foreground2.py
@@ -10,12 +10,7 @@ class bc_picture():
         self.picture = pygame.image.load(picture).convert_alpha()
         self.width=self.picture.get_size()[0]
         self.height=self.picture.get_size()[1]
-        self.bc_rect = self.picture.get_rect(topleft=(self.width, self.height))
     
-    def print_position(self,width, height):
-        rect = self.picture.get_rect(topleft=(width, height))
-        print(f'{rect.left} {rect.right} {rect.top} {rect.bottom}')
-
 class App():
     def __init__(self):
         self.isRunning=True
@@ -24,6 +19,7 @@ class App():
         self.Clock = pygame.time.Clock()
         self.background=self.set_background(PIC_FOLDER+'1.png')
         self.movingfg = self.set_moving_fg(BACKGROUNDS)
+        self.start=False
 
     def set_background(self,b_image):
         b=pygame.image.load(b_image).convert_alpha()
@@ -42,29 +38,7 @@ class App():
         c_fps = self.Clock.get_fps()
         pygame.display.set_caption(f'FPS: {c_fps : .1f}')
         
-    def draw_floor_buildings2(self):
-        padding=30
-        mid = HEIGHT/2
-        for index,pic in enumerate(self.movingfg):
-            if index == 0:
-                self.game_screen.blit(pic.picture, (pic.width*index,mid-pic.height))
-            else:
-                self.game_screen.blit(pic.picture,(pic.width*index+padding,mid-pic.height))
-            pic.print_position(padding+pic.width*index,mid-pic.height)
-
-    def draw_floor_buildings3(self):
-        padding=30
-        mid=HEIGHT//2
-        
-        for index, pic in enumerate(self.movingfg):
-            rect = pic.picture.get_rect()
-            print(f'{rect.left} {rect.right} {rect.top} {rect.bottom}')
-            if index == 0:
-                self.game_screen.blit(pic.picture, (0,mid-pic.height))
-            else:
-                self.game_screen.blit(pic.picture,(self.movingfg[index-1].width*index+padding*index,mid-pic.height))
-
-    def draw_floor_buildings(self):
+    def start_buildings(self):
         padding=30
         mid=HEIGHT//2
         prev='' 
@@ -81,8 +55,8 @@ class App():
                 prev = rect
 
     def draw(self):
-        self.game_screen.blit(self.background,(0,0))
-        self.draw_floor_buildings()
+        #self.game_screen.blit(self.background,(0,0))
+        self.start_buildings()
         pygame.display.flip()
 
     def run(self):
@@ -91,6 +65,8 @@ class App():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    print('key down')
                 self.update()
                 self.draw()
 
