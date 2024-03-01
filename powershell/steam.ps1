3b4afa0f28f93f0bd923b15b84fedcb1fc14fbd6
Carlos Diaz <cadgo.root@gmail.com>
2024-02-29 20:47:18 -0600

    Update steam.ps1

--- powershell/steam.ps1
+++ powershell/steam.ps1
@@ -5,5 +5,5 @@ $libFolder = "libraryfolders_sample.vdf"
 
 $game_path=(get-Content $libFolder | select-string -Pattern path)
 
-$nnn=$game_path[1] -replace "`t", "" -replace "`"", "" -replace "path", ""
-write-host $nnn
+#$game_list=$game_path -replace "`t", "" -replace "`"", "" -replace "path", ""
+$game_path| % {$_ -replace "`t", "" -replace "`"", "" -replace "path", ""} 
